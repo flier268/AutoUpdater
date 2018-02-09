cd8cb1c6dcfaf3235915cc95f986777eaebc1f91
Flier <flier268@gmail.com>
2018-02-10 01:25:29 +0800

    Change version

--- AutoUpdater/Properties/AssemblyInfo.cs
+++ AutoUpdater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Flier")]
 [assembly: AssemblyProduct("AutoUpdater")]
-[assembly: AssemblyCopyright("Copyright © Flier 2015")]
+[assembly: AssemblyCopyright("Copyright © Flier 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
